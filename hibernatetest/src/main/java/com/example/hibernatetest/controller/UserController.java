5e2c50f8ca645d09bda687afd9ca800392c44054
weipp <398066673@qq.com>
2019-01-17 19:38:29 +0800

    测试连接github

--- hibernatetest/src/main/java/com/example/hibernatetest/controller/UserController.java
+++ hibernatetest/src/main/java/com/example/hibernatetest/controller/UserController.java
@@ -17,19 +17,27 @@ public class UserController {
 
     @Autowired
     private UserDao userDao;
-    @ApiOperation(value = "创建用户",notes = "创建用户")
+
+    @ApiOperation(value = "创建用户", notes = "创建用户")
     @PostMapping("/saveSingle")
     @ResponseBody
-    public UserEntity saveUser(){
+    public UserEntity saveUser() {
         UserEntity userEntity = new UserEntity();
         userEntity.setName("wangwu");
         return userDao.save(userEntity);
     }
-    @ApiOperation(value = "根据用户名查询",notes = "根据用户名查询")
+
+    @ApiOperation(value = "根据用户名查询", notes = "根据用户名查询")
     @ApiParam(value = "传入用户姓名")
     @GetMapping("/findByName")
     @ResponseBody
-    public Object findByName(String name){
+    public Object findByName(String name) {
         return userDao.findByName(name);
+    }
+
+    @GetMapping("/getAll")
+    @ApiOperation(value = "查询所有用户")
+    public Object findAll() {
+        return userDao.findAll();
     }
 }
